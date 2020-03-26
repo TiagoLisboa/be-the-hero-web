9482e1ef2f132570b930179cde4020bfe1f3d318
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-25 23:56:25 -0300

    delete incident

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -31,6 +31,20 @@ export default function Profile() {
     getIncidents();
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      setIncidents(incidents.filter((incident) => incident.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -49,7 +63,7 @@ export default function Profile() {
 
       <ul>
         {incidents.map((incident) => (
-          <li key={incident._id}>
+          <li key={incident.id}>
             <strong>CASO:</strong>
             <p>{incident.title}</p>
 
@@ -64,7 +78,10 @@ export default function Profile() {
               }).format(incident.value)}
             </p>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => handleDeleteIncident(incident.id)}
+            >
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
