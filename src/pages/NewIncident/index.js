c18ccc644edf9c1c48cd286646855b46de10661a
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-25 23:10:18 -0300

    NewIncident form page

---
+++ src/pages/NewIncident/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { FiArrowLeft } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+
+import "./style.css";
+
+export default function NewIncident() {
+  return (
+    <div class="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver
+            isso.
+          </p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Volte para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="descrição" />
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">
+            Cadastar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
